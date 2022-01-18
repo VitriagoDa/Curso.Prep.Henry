5d3bb0840f02c161713d3f8ecf6110e765cec2db
VitriagoDa <vitriagodaniel17@gmail.com>
2022-01-18 17:18:17 -0500

    Tarea1.6

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -52,7 +52,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-  delete objeto[propiedad];
+  delete objeto[unaPropiedad];
 
   return objeto;
 }
