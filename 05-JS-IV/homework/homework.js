20ac2e0e12484bd15cd36709b9139b51011d78c2
VitriagoDa <vitriagodaniel17@gmail.com>
2022-01-19 10:05:59 -0500

    Tarea1.8

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -38,13 +38,12 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-   var objetoMisterioso =
-   {
+   var objetoMisterioso ={
      numeroMisterioso: 4
    }
-   var resultado = objetoMisterioso.numeroMisterioso * 5;
+  var resultado = objetoMisterioso.numeroMisterioso * 5;
 
-   return resultado;
+  return resultado;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
